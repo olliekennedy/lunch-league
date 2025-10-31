45f2606b7d59c4383a1c9012e482dd127c890185
Ollie Kennedy <ollie.kennedy1@gmail.com>
2025-10-31 11:11:31 +0000

    all hamkrest

--- src/test/kotlin/com/olliekennedy/LunchLeague.kt
+++ src/test/kotlin/com/olliekennedy/LunchLeague.kt
@@ -1,14 +1,12 @@
 package com.olliekennedy
 
 import com.natpryce.hamkrest.assertion.assertThat
-import com.natpryce.hamkrest.contains
 import com.natpryce.hamkrest.equalTo
 import org.http4k.core.ContentType.Companion.TEXT_HTML
 import org.http4k.core.Method
 import org.http4k.core.Request
 import org.http4k.core.Status
 import org.http4k.lens.contentType
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class LunchLeague {
@@ -16,7 +14,7 @@ class LunchLeague {
     @Test
     fun `home shows the leaderboard`() {
         val response = app(Request(Method.GET, "/"))
-        assertEquals(Status.OK, response.status)
+        assertThat(Status.OK, equalTo(response.status))
         assertThat(response.contentType(), equalTo(TEXT_HTML))
         val title = response.bodyString()
         assertThat(title, equalTo("Lunch League"))
@@ -25,7 +23,7 @@ class LunchLeague {
     @Test
     fun `vote shows a voting page`() {
         val response = app(Request(Method.GET, "/vote"))
-        assertEquals(Status.OK, response.status)
+        assertThat(Status.OK, equalTo(response.status))
         assertThat(response.contentType(), equalTo(TEXT_HTML))
         val title = response.bodyString()
         assertThat(title, equalTo("Place your vote:"))
