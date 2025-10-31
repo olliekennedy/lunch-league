5ebec5acb139b2406cb54548b41355370da50813
Ollie Kennedy <ollie.kennedy1@gmail.com>
2025-10-31 12:44:28 +0000

    convert to handlebars

--- src/main/kotlin/com/olliekennedy/LunchLeague.kt
+++ src/main/kotlin/com/olliekennedy/LunchLeague.kt
@@ -1,32 +1,35 @@
 package com.olliekennedy
 
+import org.http4k.core.Body
+import org.http4k.core.ContentType.Companion.TEXT_HTML
 import org.http4k.core.HttpHandler
 import org.http4k.core.Method
 import org.http4k.core.Response
 import org.http4k.core.Status
 import org.http4k.core.then
+import org.http4k.core.with
 import org.http4k.filter.DebuggingFilters.PrintRequest
-import org.http4k.lens.html
 import org.http4k.routing.bind
 import org.http4k.routing.routes
 import org.http4k.server.Jetty
 import org.http4k.server.asServer
+import org.http4k.template.HandlebarsTemplates
+import org.http4k.template.ViewModel
+import org.http4k.template.viewModel
+
+object HomePage : ViewModel
+object VotePage : ViewModel
+
+val renderer = HandlebarsTemplates().CachingClasspath()
+val view = Body.viewModel(renderer, TEXT_HTML).toLens()
 
 val app = routes(
     "/" bind Method.GET to {
-        Response(Status.OK).html(
-            """
-                <h1>Lunch League</h1>
-            """.trimIndent())
+        Response(Status.OK).with(view of HomePage)
     },
 
     "/vote" bind Method.GET to {
-        Response(Status.OK).html(
-            """
-                <h1>Lunch League</h1>
-                <h2>Place your vote:</h2>
-            """.trimIndent()
-        )
+        Response(Status.OK).with(view of VotePage)
     },
 )
 
